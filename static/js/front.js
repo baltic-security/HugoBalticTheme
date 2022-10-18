f523465ce7744f7bc984e966a3e93c31c7e2b76f
Andreis Purim <andreispurim@gmail.com>
2022-10-18 17:28:28 +0300

    Added animated logo but having trouble making the navbar transparent the correct way

--- static/js/front.js
+++ static/js/front.js
@@ -43,40 +43,6 @@ function contactFormAjax () {
   })
 }
 
-/* for demo purpose only - can be deleted */
-function demo () {
-  if ($.cookie('themeCSSpath')) {
-    $('link#theme-stylesheet').attr('href', $.cookie('themeCSSpath'))
-  }
-
-  $('#colour').change(function () {
-    if ($(this).val() !== '') {
-      var themeCSSpath = 'css/style.' + $(this).val() + '.css'
-
-      $('link#theme-stylesheet').attr('href', themeCSSpath)
-
-      $.cookie('themeCSSpath', themeCSSpath, {expires: 365, path: '/'})
-    }
-
-    return false
-  })
-
-  $('#layout').change(function () {
-    if ($(this).val() !== '') {
-      var themeLayout = $(this).val()
-
-      $('body').removeClass('wide')
-      $('body').removeClass('boxed')
-
-      $('body').addClass(themeLayout)
-
-      $.cookie('themeLayout', themeLayout, {expires: 365, path: '/'})
-    }
-
-    return false
-  })
-}
-
 /* slider homepage */
 function sliderHomepage () {
   if ($('#slider').length) {
